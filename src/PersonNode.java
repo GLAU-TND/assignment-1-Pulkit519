a362fae89f4c60eab0b043014622cc47d4e5ae24
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-02-24 20:59:21 +0530

    fields created : person and next with getter and setter

--- src/PersonNode.java
+++ src/PersonNode.java
@@ -1,2 +1,29 @@
 public class PersonNode {
+    private Person person;
+    private PersonNode next;
+
+    public PersonNode(Person person) {
+        this.person = person;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public PersonNode getNext() {
+        return next;
+    }
+
+    public void setNext(PersonNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return person.toString();
+    }
 }
