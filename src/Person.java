201ef042bada92f21976e7a201c08fe77e37cfc3
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-02-24 20:57:40 +0530

    getter ,  setter and helper methods are created

--- src/Person.java
+++ src/Person.java
@@ -6,4 +6,60 @@ public class Person {
     private String temp;
     private String emai;
     private ArrayList<String> numbers = new ArrayList<>();
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public void setNumber(String n) {
+        numbers.add(n);
+    }
+
+    public void showNumber() {
+        if (numbers.size() > 1) {
+            System.out.print("Contact Number(s): ");
+        } else {
+            System.out.print("Contact Number: ");
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+            if (i != numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public void showPerson() {
+        System.out.println("-------- * -------- * -------- * --------");
+        System.out.println("First Name: " + first);
+        System.out.println("Last Name: " + last);
+        showNumber();
+        System.out.println("\n" + "Email address: " + emai);
+        System.out.println("-------- * -------- * -------- * --------");
+    }
+
+    @Override
+    public String toString() {
+        return first;
+    }
 }
