e7fc0975721394b7aef7ad0e681888d70499950a
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-02-24 20:57:00 +0530

    fields created : first, last, temp, emai, numbers

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,9 @@
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+    private String first;
+    private String last;
+    private String temp;
+    private String emai;
+    private ArrayList<String> numbers = new ArrayList<>();
 }
