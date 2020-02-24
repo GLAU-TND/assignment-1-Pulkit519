4558ab41ee57098fc79c1808efa1f572e6505d10
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-02-24 21:06:15 +0530

    half tested code is ready for check please evaluate it

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Contacts c = new Contacts();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to DBC's Contact List App");
+        System.out.println("Welcome to Pulkit's Contact List App");
         System.out.println("Press 1 to add a new contact");
         System.out.println("Press 2 to view all contacts");
         System.out.println("Press 3 to search for a contact");
